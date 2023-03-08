ea2f1487e2b8d50411d9b1e3a1cc6c0211756d75
VictorGugg <victor.guggenbuhl@gmail.com>
2023-03-08 15:04:14 +0100

    Initial commit. Single-person project to practice with React.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+// function App() { = ligne 5
+const App = () => {
+  const [counter, setCounter] = useState(0); // 0 = default value, could be anything (int, string etc)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {/* ... */}
+        <p>Currently at {counter}</p>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
       </header>
     </div>
   );
